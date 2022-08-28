fe884def6e09cba3fbf4bbe92c87455611851a09
Julien Rolland <rollandjulien13013@gmail.com>
2022-08-28 22:29:53 +0200

    styling shop and product cards

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,12 +1,24 @@
+import { Link } from "react-router-dom";
+import emerald from '../assets/emerald.png';
+
 export const ProductCard = ({name, description, price, img_path, article_number}) => {
 
 
   return (
     <div className="product-card">
-      <img src={img_path} alt="" />
-      <h5>{name}</h5>
-      <p>{description}</p>
-      <span>{price}</span>
+      <div className="product-card-img">
+        <img src={img_path} alt="" />
+      </div>
+      <div className="product-card-info">
+        <Link to={`/product/${article_number}`}>
+          <h5>{name}</h5>
+        </Link>
+        <p>{description}</p>
+      </div>
+      <div className="product-card-price">
+        <span>{price}</span>
+        <img src={emerald} alt="" />
+      </div>
     </div>
   )
 }
