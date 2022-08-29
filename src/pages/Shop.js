a6a05ef9b8d177fefcda3f7e092d9ae26f33fff5
Julien Rolland <rollandjulien13013@gmail.com>
2022-08-29 20:19:45 +0200

    setup max price filter

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -6,6 +6,7 @@ import { Categories } from "../components/Categories";
 
 export function Shop(props) {
   const [products, setProducts] = useState([]);
+  const [maxPrice,setMaxPrice] = useState('');
 
 
     useEffect(() => {
@@ -13,7 +14,18 @@ export function Shop(props) {
         .then(res => {
           setProducts(res.data)
         }).catch(err => console.log(err))
-    }, [])
+    }, []);
+
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      try {
+        const response = await axios.get(`https://miner-api.herokuapp.com/price?max=${maxPrice}`);
+        setProducts(response.data);
+      } catch (err) {
+        console.log(err);
+      }
+    }
 
     const listProducts = products.map((product) =>
         <ProductCard key={product.article_number}
@@ -32,10 +44,11 @@ export function Shop(props) {
           <Categories />
 
           <div className="side-bar-price">
-            <form>
+            <form onSubmit={handleSubmit}>
               <label htmlFor="price">Max-price</label>
               <input type="number" id="price" name="price"
-                     min="0" max="100" step="10"/>
+                     min="0" max="100" step="10"
+                     onChange={(e) => setMaxPrice(e.target.value)}/>
               <button>Filter</button>
             </form>
           </div>
