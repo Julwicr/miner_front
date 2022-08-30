e76a3b504aff46af08950183204192ef6b7173dd
Julien Rolland <rollandjulien13013@gmail.com>
2022-08-30 13:36:14 +0200

    check if user logged in on landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,14 +1,21 @@
 import { LoginForm } from "../components/LoginForm"
 import { useState } from 'react';
+import Cookies from "js-cookie";
+import { Link } from "react-router-dom"
 
 export function Landing() {
   const [formIsShown, setFormIsShown] = useState(false);
-  
+
   return (
     <div className="landing">
       <h2>Welcome to our private sales.</h2>
       <LoginForm isShown={formIsShown} setIsShown={() => setFormIsShown(false)}/>
-      <button className="enter-btn" onClick={() => setFormIsShown(true)}>Enter</button>
+      {
+        Cookies.get('loggedIn') ?
+        <Link to="/shop" className="enter-btn">Enter</Link> :
+
+        <button className="enter-btn" onClick={() => setFormIsShown(true)}>Log In</button>
+      }
       <h3>The right tools for the best miner in town !</h3>
     </div>
   );
